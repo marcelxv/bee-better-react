2fdb89a60dc6822340b83f827d806f07dd952170
Marcel Scognamiglio <67491786+marcelxv@users.noreply.github.com>
2022-11-30 16:45:46 -0300

    feat: improves form validation

--- src/components/UserCollectForm.tsx
+++ src/components/UserCollectForm.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 export default function UserCollectForm({
   step,
@@ -11,170 +11,163 @@ export default function UserCollectForm({
   setPoints: any;
 }) {
   const collectPointContainerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    width: "100%",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: '100%',
   };
   const garbageBagContainerStyle = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   };
 
   const summaryTableStyle = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    width: "200px",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    width: '200px',
   };
 
   const summaryTableRowStyle = {
-    border: "1px solid black",
-    width: "100%",
-    height: "100%",
-    margin: "1px",
+    border: '1px solid black',
+    width: '100%',
+    height: '100%',
+    margin: '1px',
   };
 
   const totalTableStyle = {
-    backgroundColor: "lightgrey",
-    width: "100%",
-    height: "100%",
-    margin: "1px",
-  };
-
-  const garbageBagCardStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "300px",
-    margin: "10px",
-    padding: "20px 0",
-    backgroundColor: "#F5F5F5",
-    borderRadius: "10px",
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    backgroundColor: 'lightgrey',
+    width: '100%',
+    height: '100%',
+    margin: '1px',
   };
 
   const formInputStyle = {
-    width: "50px",
-    height: "40px",
-    margin: "10px",
-    padding: "0 0.4em",
-    border: "none",
-    borderRadius: "10px",
-    backgroundColor: "#F5F5F5",
+    width: '50px',
+    height: '40px',
+    margin: '10px',
+    padding: '0 0.4em',
+    border: 'none',
+    borderRadius: '10px',
+    backgroundColor: '#F5F5F5',
   };
 
-  const [CEP, setCEP] = useState("");
+  const [CEP, setCEP] = useState('');
   const [collectPoints, setCollectPoints] = useState([
     {
       id: 1,
-      name: "Ponto de Coleta 1",
-      address: "Rua 1, 123",
-      city: "São Paulo",
-      state: "SP",
-      CEP: "12345-678",
+      name: 'Ponto de Coleta 1',
+      address: 'Rua 1, 123',
+      city: 'São Paulo',
+      state: 'SP',
+      CEP: '12345-678',
     },
     {
       id: 2,
-      name: "Ponto de Coleta 2",
-      address: "Rua 2, 456",
-      city: "São Paulo",
-      state: "SP",
-      CEP: "12345-678",
+      name: 'Ponto de Coleta 2',
+      address: 'Rua 2, 456',
+      city: 'São Paulo',
+      state: 'SP',
+      CEP: '12345-678',
     },
     {
-      id: 2,
-      name: "Ponto de Coleta 2",
-      address: "Rua 2, 456",
-      city: "São Paulo",
-      state: "SP",
-      CEP: "12345-678",
+      id: 3,
+      name: 'Ponto de Coleta 2',
+      address: 'Rua 2, 456',
+      city: 'São Paulo',
+      state: 'SP',
+      CEP: '12345-678',
     },
     {
       id: 8,
-      name: "Ecoloja",
-      address: "Rua 2, 456",
-      city: "São Paulo",
-      state: "SP",
-      CEP: "12345-678",
+      name: 'Ecoloja',
+      address: 'Rua 2, 456',
+      city: 'São Paulo',
+      state: 'SP',
+      CEP: '12345-678',
     },
     {
       id: 12,
-      name: "Ecologia",
-      address: "Rua 2, 456",
-      city: "São Paulo",
-      state: "SP",
-      CEP: "12345-678",
+      name: 'Ecologia',
+      address: 'Rua 2, 456',
+      city: 'São Paulo',
+      state: 'SP',
+      CEP: '12345-678',
     },
     {
       id: 20,
-      name: "Ponto de Coleta 44",
-      address: "Rua 2, 456",
-      city: "São Paulo",
-      state: "SP",
-      CEP: "12345-678",
+      name: 'Ponto de Coleta 44',
+      address: 'Rua 2, 456',
+      city: 'São Paulo',
+      state: 'SP',
+      CEP: '12345-678',
     },
   ]);
 
   const [selectedCollectPoint, setSelectedCollectPoint] = useState({
     id: 0,
-    name: "",
-    address: "",
-    city: "",
-    state: "",
-    CEP: "",
+    name: '',
+    address: '',
+    city: '',
+    state: '',
+    CEP: '',
   });
 
   const [bagCounter, setBagCounter] = useState(1);
 
+  const [isValid, setIsValid] = useState(false);
+
   const [garbageBags, setGarbageBags] = useState([
     {
-      id: 1,
-      bagType: "",
-      bagWeight: 0,
+      id: 0 as number,
+      bagType: '' as string,
+      bagWeight: 0 as number,
     },
   ]);
 
   const [totalWeight, setTotalWeight] = useState(0);
 
   const collectPointStyle = {
-    height: "100px",
-    padding: "10px",
-    backgroundColor: "#fff",
-    fontSize: "0.7rem",
-    cursor: "pointer",
-    borderRadius: "10px",
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-    margin: "10px",
+    height: '100px',
+    padding: '10px',
+    backgroundColor: '#fff',
+    fontSize: '0.7rem',
+    cursor: 'pointer',
+    borderRadius: '10px',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    margin: '10px',
   };
 
   const handlePoints = () => {
-    setPoints(points as any + totalWeight);
+    setPoints((points as any) + totalWeight);
   };
 
   const handleSelectCollectPoint = (collectPoint: any) => {
     setSelectedCollectPoint(collectPoint);
   };
 
-  const handleGarbageBags = (bagCounter: number) => {
-    setBagCounter(bagCounter);
-    const garbageBags = [];
-    for (let i = 0; i < bagCounter; i++) {
-      garbageBags.push({
-        id: i,
-        bagType: "",
-        bagWeight: 0,
-      });
+  const handleGarbageBags = (action: string) => {
+    if (action === 'add') {
+      setBagCounter(bagCounter + 1);
+      setGarbageBags([
+        ...garbageBags,
+        {
+          id: bagCounter,
+          bagType: '',
+          bagWeight: 0,
+        },
+      ]);
+    } else if (action === 'remove') {
+      setBagCounter(bagCounter ? bagCounter - 1 : 0);
+      setGarbageBags(garbageBags.slice(0, garbageBags.length - 1));
     }
-    setGarbageBags(garbageBags);
   };
 
   const handleGarbageBagType = (e: any, bagId: number) => {
@@ -188,13 +181,52 @@ export default function UserCollectForm({
     garbageBagsCopy[bagId].bagWeight = e.target.value;
     setGarbageBags(garbageBagsCopy);
     setTotalWeight(
-      garbageBagsCopy.reduce((acc, bag) => acc + +bag.bagWeight, 0)
+      Math.round(
+        garbageBagsCopy.reduce((acc: number, bag: any) => {
+          return acc + +bag.bagWeight;
+        }, 0) * 100
+      ) / 100
     );
   };
 
+  const handleGarbageCardStyle = (bagType: string) => {
+    const bagTypeStyle = {
+      backgroundColor: '#F5F5F5',
+      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      borderRadius: '10px',
+      padding: '10px 30px',
+      margin: '10px',
+    };
+
+    switch (bagType) {
+      case 'papel':
+        return {
+          ...bagTypeStyle,
+          backgroundColor: 'yellow',
+        };
+      case 'plastico':
+        return {
+          ...bagTypeStyle,
+          backgroundColor: 'blue',
+          color: 'white',
+        };
+      default:
+        return {
+          ...bagTypeStyle,
+        };
+    }
+  };
+
+  useEffect(() => {
+    const allBagsHaveParams = garbageBags.every(
+      (bag) => bag.bagType && bag.bagWeight > 0
+    );
+    setIsValid(allBagsHaveParams);
+  }, [garbageBags]);
+
   return (
     <div>
-      {step === "setCEP" && (
+      {step === 'setCEP' && (
         <div style={collectPointContainerStyle as React.CSSProperties}>
           <h1>Qual o seu CEP?</h1>
           <input
@@ -208,10 +240,10 @@ export default function UserCollectForm({
           </Link>
         </div>
       )}
-      {step === "selecao" && (
+      {step === 'selecao' && (
         <div style={collectPointContainerStyle as React.CSSProperties}>
           <h1>Selecione o posto de coleta mais próximo</h1>
-          <h3>{selectedCollectPoint ? selectedCollectPoint.name : ""}</h3>
+          <h3>{selectedCollectPoint ? selectedCollectPoint.name : ''}</h3>
           <div style={collectPointContainerStyle as React.CSSProperties}>
             {collectPoints.map((collectPoint) => (
               <div
@@ -229,7 +261,7 @@ export default function UserCollectForm({
           </Link>
         </div>
       )}
-      {step === "formulario" && (
+      {step === 'formulario' && (
         <div>
           <div>
             <h3>separação de lixo para reciclagem</h3>
@@ -245,17 +277,14 @@ export default function UserCollectForm({
               <h4>Sacos</h4>
               <input
                 style={formInputStyle as React.CSSProperties}
-                type="text"
+                type="number"
+                min="0"
+                step="1"
                 title="sacos"
                 value={bagCounter}
-                onChange={() => handleGarbageBags(bagCounter)}
               />
-              <button onClick={() => handleGarbageBags(bagCounter + 1)}>
-                +
-              </button>
-              <button onClick={() => handleGarbageBags(bagCounter - 1)}>
-                -
-              </button>
+              <button onClick={() => handleGarbageBags('add')}>+</button>
+              <button onClick={() => handleGarbageBags('remove')}>-</button>
             </div>
           </div>
           <div>
@@ -265,17 +294,18 @@ export default function UserCollectForm({
             {garbageBags.map((garbageBag) => (
               <div
                 key={garbageBag.id}
-                style={garbageBagCardStyle as React.CSSProperties}
+                style={handleGarbageCardStyle(garbageBag.bagType)}
               >
-                <h4>Saco {garbageBag.id}</h4>
+                <h4>Saco</h4>
                 <div>
-                  <h5>Tipo</h5>
+                  <label htmlFor="bagType">Tipo</label>
                   <select
                     title="tipo"
                     name="bagType"
                     id="bagType"
                     value={garbageBag.bagType}
                     onChange={(e) => handleGarbageBagType(e, garbageBag.id)}
+                    required
                   >
                     <option value="">Selecione</option>
                     <option value="papel">Papel</option>
@@ -286,15 +316,20 @@ export default function UserCollectForm({
                   </select>
                 </div>
                 <div>
-                  <h5>Peso</h5>
+                  <label htmlFor="bagWeight">Peso</label>
                   <input
                     style={formInputStyle as React.CSSProperties}
-                    type="texto"
+                    type="number"
+                    step="0.1"
+                    min="0"
                     title="peso"
+                    name="bagWeight"
+                    id="bagWeight"
                     value={garbageBag.bagWeight}
                     onChange={(e) => {
                       handleGarbageBagWeight(e, garbageBag.id);
                     }}
+                    required
                   />
                   <span>kgs</span>
                 </div>
@@ -302,11 +337,11 @@ export default function UserCollectForm({
             ))}
           </div>
           <Link to="/activity/reciclagem/checkout">
-            <button>Próximo</button>
+            <button disabled={!isValid}>Próximo</button>
           </Link>
         </div>
       )}
-      {step === "checkout" && (
+      {step === 'checkout' && (
         <div>
           <h1>Confirmação</h1>
           <h3>{selectedCollectPoint.name}</h3>
@@ -337,7 +372,7 @@ export default function UserCollectForm({
           </div>
         </div>
       )}
-      {step === "confirmacao" && (
+      {step === 'confirmacao' && (
         <div>
           <h1>Confirmação</h1>
           <p>
