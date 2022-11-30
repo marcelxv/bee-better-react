acb82c604caa2acd9309f04c2d0c63dd06f2e0eb
Marcel Scognamiglio <67491786+marcelxv@users.noreply.github.com>
2022-11-30 16:46:10 -0300

    style: improves ternary

--- src/pages/ActivityPage.tsx
+++ src/pages/ActivityPage.tsx
@@ -1,6 +1,6 @@
-import UserCoinCounter from "../components/UserCoinCounter";
-import UserCollectForm from "../components/UserCollectForm";
-import { useState } from "react";
+import UserCoinCounter from '../components/UserCoinCounter';
+import UserCollectForm from '../components/UserCollectForm';
+import { useState } from 'react';
 
 export default function ActivityPage({
   pageType,
@@ -9,29 +9,29 @@ export default function ActivityPage({
   pageType: string;
   step: string;
 }) {
-  const activityPageContainerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#F5F5F5",
+  const pageContainerStyle = {
+    height: '100vh',
+    backgroundColor: 'yellow',
+  };
+
+  const activityContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#F5F5F5',
   };
 
   const [points, setPoints] = useState(0);
 
-  const reciclagemPageMount = () => {
-    if (pageType === "reciclagem") {
-      return (
-        <div>
+  return (
+    <div style={pageContainerStyle as React.CSSProperties}>
+      {pageType === 'reciclagem' ? (
+        <div style={activityContainerStyle as React.CSSProperties}>
           <UserCollectForm step={step} points={points} setPoints={setPoints} />
         </div>
-      );
-    }
-  };
-  return (
-    <div style={activityPageContainerStyle as React.CSSProperties}>
-      {reciclagemPageMount()}
+      ) : null}
       <UserCoinCounter points={points} />
     </div>
   );
